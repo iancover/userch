2cf2116c439d74c45b9e940fb4d388dad14641aa
Ian Cover <iancover@gmail.com>
2021-12-25 13:51:01 -0600

    feat: Import Router and Navbar component
    Add Navbar component inside a flex div wrapped in a Router element
    to create routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
