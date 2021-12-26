5b650538a4746809260cb2f656b4a097107b2c9c
Ian Cover <iancover@gmail.com>
2021-12-26 09:06:32 -0600

    feat: Add Home, About & NotFound Routes
    Routes '/' & '/about' render Home & About components and the '*' is
    a catch all which will render the NotFound component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
@@ -8,7 +11,13 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className={'container mx-auto px-3 pb-12'}>Content</main>
+        <main className={'container mx-auto px-3 pb-12'}>
+          <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'about'} element={<About />} />
+            <Route path={'*'} element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
