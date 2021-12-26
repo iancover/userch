737f1f37448a202008f39dc8c990afa7508ad4df
Ian Cover <iancover@gmail.com>
2021-12-26 10:16:51 -0600

    feat: Add Route components Home, About & NotFound
    '/' routes to Home, '/about' to About component which displays
    a description of the application and a catch all route '/*' which
    for the NotFound component which displays a 404 Not Found page.

---
+++ src/pages/About.jsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+
+function About() {
+  const reactLink = 'https://reactjs.org/docs/getting-started.html#try-react';
+  const githubLink = 'https://docs.github.com/en/rest';
+  const craLink = 'https://create-react-app.dev/docs/getting-started/';
+  const tailwindLink = 'https://tailwindcss.com/';
+  const daisyuiLink = 'https://daisyui.com/';
+
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Userch: GitHub</h1>
+      <p className='font-light text-2xl mb-4'>
+        A{' '}
+        <Link to={reactLink} className={'text-sky-400'}>
+          React
+        </Link>{' '}
+        app bootstrapped with{' '}
+        <Link to={craLink} className={'text-sky-400'}>
+          Create React App
+        </Link>{' '}
+        and styled using the{' '}
+        <Link to={tailwindLink} className={'text-sky-400'}>
+          TailwindCSS
+        </Link>{' '}
+        library and one of its plugins,{' '}
+        <Link to={daisyuiLink} className={'text-sky-400'}>
+          daisyUI
+        </Link>
+        . This project currently uses the{' '}
+        <Link to={githubLink} className={'text-sky-400'}>
+          GitHub REST API
+        </Link>{' '}
+        to search user profiles and repo information.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>2.0.0</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import { BiError } from 'react-icons/bi';
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div className={'hero'}>
+      <div className='text-center hero-content'>
+        <div className='max-w-lg'>
+          <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
+          <div className='text-5xl mb-8'>
+            <BiError size={'4rem'} className={'inline-block align-text-top text-yellow-600 mr-2'}/> 
+            404 Page Not Found!
+          </div>
+          <Link to={'/'} className={'btn btn-lg bg-cyan-800'}>
+            <FaHome className={'mr-2'} />
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
