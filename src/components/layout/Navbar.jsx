4ba57f00d751dd18f0d3cfd3db0d15c696687965
Ian Cover <iancover@gmail.com>
2021-12-26 09:04:44 -0600

    No changes nothing to commit

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,8 +13,8 @@ function Navbar({ title }) {
           </Link>
         </div>
 
-        <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
             <Link to={'/'} className={'btn btn-ghost btn-sm rounded-btn'}>
               Home
             </Link>
