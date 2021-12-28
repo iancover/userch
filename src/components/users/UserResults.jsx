63b349723410f50f746669ed217bdc73447449f7
Ian Cover <iancover@gmail.com>
2021-12-28 13:26:35 -0600

    feat: Create component to fetch users to GitHub API
    - useEffect() to fetch users from GitHub API when component loads
      asynchronously using Fetch API 'fetch()' sending the url and token
      stored on .env file.
    
    - useState() to set data from fetched response as state with setUsers
      and a loading state with setLoading to display a loader img as common
      practice when making request to an API.

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await res.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  return <div>User Results</div>;
+}
+
+export default UserResults;
