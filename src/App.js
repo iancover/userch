4f70c3c201a307466bd8776551d79acc0f184472
Ian Cover <iancover@gmail.com>
2021-12-29 08:52:25 -0600

    style: Increase <main> margin-top to 10px
    Add class mt-10 to <main> object to allow space for <nav> since it
    is taken out of normal flow with fixed position to allow scrolling.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className={'container mx-auto px-3 pb-12'}>
+        <main className={'container mx-auto mt-10 px-3 pb-12'}>
           <Routes>
             <Route path={'/'} element={<Home />} />
             <Route path={'about'} element={<About />} />
