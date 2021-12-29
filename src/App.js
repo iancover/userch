33629954ab8dacec0e459eda7a3b293e3d148330
Ian Cover <iancover@gmail.com>
2021-12-29 13:38:12 -0600

    refactor: Replace useContext for useState hook
    UserResults.jsx
    With Context Provider <GithubContext.Provider> we can manage
    state from context making the request to the Github API from there
    and injecting the data directly so can transfer setUsers and setLoading
    along with fetchUsers to Context module and pass them thru Context
    Provider as values, which we import with useContext().
    
    App.js
    Add <GithubContext.Provider> wrapper to pass values to any of its
    children components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+
+// context
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className={'container mx-auto mt-20 px-3 pb-12'}>
-          <Routes>
-            <Route path={'/'} element={<Home />} />
-            <Route path={'about'} element={<About />} />
-            <Route path={'*'} element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className={'container mx-auto mt-20 px-3 pb-12'}>
+            <Routes>
+              <Route path={'/'} element={<Home />} />
+              <Route path={'about'} element={<About />} />
+              <Route path={'*'} element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,32 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from '../users/UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-    setUsers(data);
-    setTimeout(() => setLoading(false), 2000);
-  };
-
   if (!loading) {
     return (
       <div className={'grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'}>
-        {users.map((user) => (
-          <UserItem key={user.id} user={user}/>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
