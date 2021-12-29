58f5e750c6da37644c68fd91bf01eb30a2a5df39
Ian Cover <iancover@gmail.com>
2021-12-29 09:53:15 -0600

    style: Adust z-index to nav with z-10 class

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function Navbar({ title }) {
   return (
-    <nav className={'navbar mb-12 shadow-lg bg-neutral text-neutral-content fixed min-w-full'}>
+    <nav className={'navbar mb-12 shadow-lg bg-neutral text-neutral-content fixed min-w-full z-10'}>
       <div className='container mx-auto'>
         <div className='flex-none px-2 mx-2'>
           <FaGithub className={'inline pr-2 text-3xl'} />
