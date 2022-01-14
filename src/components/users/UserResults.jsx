d1f85afcf1db72f9cebf34bb47d236784c082901
Ian Cover <iancover@gmail.com>
2022-01-14 04:27:02 -0600

    style: Rearrange grid classes by screen size

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -8,7 +8,7 @@ function UserResults() {
 
   if (!loading) {
     return (
-      <div className={'grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'}>
+      <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'}>
         {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
