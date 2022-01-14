6fb8ff156fa71d22825edff3724017fe99fb7bf9
Ian Cover <iancover@gmail.com>
2022-01-14 04:43:05 -0600

    refactor & style: Handle Clear btn functionality
    Display 'Clear' button if there are users rendered and handle
    button functionality onClick by importing 'clearUsers' from context.
    
    style: Adjust inputs and buttons grid classes if displaying based
    on screen size.

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -3,14 +3,14 @@ import GithubContext from '../../context/github/GithubContext';
 
 function UserSearch() {
   const [text, setText] = useState('');
-  const { users, searchUsers } = useContext(GithubContext);
+  const { users, searchUsers, clearUsers } = useContext(GithubContext);
 
   const handleChange = e => setText(e.target.value);
 
   const handleSubmit = e => {
     e.preventDefault();
     if (text === '') {
-      alert('');  
+      alert('');
     } else {
       searchUsers(text);
       setText('');
@@ -18,27 +18,62 @@ function UserSearch() {
   };
 
   return (
-    <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 mb-8'}>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
-              <input
-                type='text'
-                className='input input-lg bg-gray-200 w-full pr-40 text-black'
-                value={text}
-                onChange={handleChange}
-                placeholder={'Search'}
-                autoFocus={true}
-              />
-              <button className='btn btn-lg w-36 absolute top-0 right-0 rounded-l-none'>GO</button>
-            </div>
+    <div
+      className={
+        'grid grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-6 sm:gap-8 mb-8'
+      }>
+      {users.length > 0 ? (
+        <>
+          <div className={'col-span-2 md:col-span-3 lg:col-span-3 xl:col-span-3'}>
+            <form onSubmit={handleSubmit}>
+              <div className='form-control'>
+                <div className='relative'>
+                  <input
+                    type='text'
+                    className={'input input-lg w-full pr-30 text-black bg-gray-200'}
+                    value={text}
+                    onChange={handleChange}
+                    placeholder={'Search'}
+                    autoFocus={true}
+                  />
+                  <button
+                    className={
+                      'btn btn-lg absolute top-0 right-0 w-24 md:w-36 lg:w-44 rounded-l-none'
+                    }>
+                    GO
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      {users.length > 0 && (
-        <div>
-          <button className='btn btn-ghost btn-lg'>Clear</button>
+          <div className={'col-span-1'}>
+            <button className='btn btn-ghost btn-lg min-w-full' onClick={clearUsers}>
+              Clear
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className={'col-span-3 md:col-span-4 lg:col-span-4 xl:col-span-4'}>
+          <form onSubmit={handleSubmit}>
+            <div className='form-control'>
+              <div className='relative'>
+                <input
+                  type='text'
+                  className={'input input-lg w-full text-black bg-gray-200'}
+                  value={text}
+                  onChange={handleChange}
+                  placeholder={'Search'}
+                  autoFocus={true}
+                />
+                <button
+                  className={
+                    'btn btn-lg absolute top-0 right-0 w-24 sm:w-36 lg:w-48 rounded-l-none'
+                  }>
+                  GO
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       )}
     </div>
