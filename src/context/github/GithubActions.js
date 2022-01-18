dec0a1453158417c1a6ed53b56ab5c0773a6b13d
Ian Cover <iancover@gmail.com>
2022-01-18 08:48:57 -0600

    refactor(api): Add axios instead of using fetchAPI
    Add axios to simplify code, its ok to use FetchAPI but with axios
    we can instantiate a request object with axios.create() and config
    GithubAPI and headers once.

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,38 +1,26 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+});
+
 export const searchUsers = async text => {
-  // remove setLoading since we're gonna dispatch directly from component
   const params = new URLSearchParams({ q: text });
-  const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: { Authorization: `token ${GITHUB_TOKEN}` },
-  });
-  const { items } = await res.json();
-  return items;
+  const res = await github.get(`/search/users?${params}`);
+  return res.data.items;
 };
 
 export const getUser = async login => {
-  const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-    headers: { Authorization: `token ${GITHUB_TOKEN}` },
-  });
-  
-  if (res.status === 404) {
-    window.location = '/notfound';
-  } else {
-    const data = await res.json();
-    return data;
-  }
+  const res = await github.get(`/users/${login}`);
+  return res.status === 404 ? (window.location = '/notfound') : res.data;
 };
 
 export const getUserRepos = async login => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10
-  })
-
-  const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    headers: { Authorization: `token ${GITHUB_TOKEN}` },
-  });
-  const data = await res.json();
-  return data;
+  const params = new URLSearchParams({ sort: 'created', per_page: 10 });
+  const res = await github.get(`/users/${login}/repos?${params}`);
+  return res.data;
 };
